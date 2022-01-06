5013aaf8b70ecbf252986eba8cc5f29d68c1da65
LUM Adeline <90911523+ALIDA126@users.noreply.github.com>
2022-01-06 11:24:14 +0100

    editvehicle

--- src/Form/EditVehicleType.php
+++ src/Form/EditVehicleType.php
@@ -2,9 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\Vehicle;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class EditVehicleType extends AbstractType
 {
@@ -57,7 +66,7 @@ class EditVehicleType extends AbstractType
             'required' => true,
             'attr' => ['placeholder' => 'Entrez le carburant utiliser']
         ])
-        ->add('picture', FileType::class, [
+        ->add('picture', TextType::class, [
             'label' => 'Photo',
             'required' => true,
             'attr' => ['placeholder' => 'Entrez votre photo']
